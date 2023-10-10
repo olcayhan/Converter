6d19fc49e7f4de18934055354c10eee2eafc8138
olcayhank <kolcayhan1@gmail.com>
2023-10-10 22:25:04 +0300

    created zip download and multi images

--- client/components/fileUpload.tsx
+++ client/components/fileUpload.tsx
@@ -17,9 +17,10 @@ enum fileTypes {
 }
 
 const FileUpload = () => {
-  const [base64, setBase64] = useState<ImageData | undefined>(undefined);
-  const [image, setImage] = useState<string | undefined>(undefined);
-  const [type, setType] = useState<string>(fileTypes[0]);
+  const [base64, setBase64] = useState<ImageData[]>([]);
+  const [image, setImage] = useState<ImageData[]>([]);
+  const [type, setType] = useState<string>(fileTypes[2]);
+  const [zipped, setZipped] = useState<string | undefined>(undefined);
 
   const typeArray: string[] = Object.keys(fileTypes).filter((key) =>
     isNaN(Number(key))
@@ -28,34 +29,41 @@ const FileUpload = () => {
   const onDrop = useCallback((acceptedFiles: any) => {
     if (acceptedFiles.length) {
       try {
-        const reader = new FileReader();
-
-        reader.onload = (e: any) => {
-          const base64Data = e.target.result;
-          setBase64({ name: acceptedFiles[0].name, src: base64Data });
-        };
-
-        reader.readAsDataURL(acceptedFiles[0]);
+        acceptedFiles.forEach((acceptedFile: any) => {
+          const reader = new FileReader();
+          reader.onload = (e: any) => {
+            setBase64((prevBase64) => [
+              ...prevBase64,
+              { name: acceptedFile.name, src: e.target.result },
+            ]);
+          };
+          reader.readAsDataURL(acceptedFile);
+        });
       } catch (err) {
         console.log(err);
       } finally {
-        setImage(undefined);
+        setImage([]);
       }
     }
   }, []);
 
-  const handleConvert = async () => {
+  const handleConvert = () => {
     try {
-      const image = await fetch(
-        `http://localhost:5000/convert/upload/${type}`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ data: base64?.src }),
-        }
-      );
-      const data = await image.json();
-      setImage(data);
+      base64.forEach(async (base64Item) => {
+        const image = await fetch(
+          `http://localhost:5000/convert/upload/${type}`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ data: base64Item?.src }),
+          }
+        );
+        const data = await image.json();
+        setImage((prevImage) => [
+          ...prevImage,
+          { name: base64Item.name.split(".")[0] + `.${type}`, src: data.src },
+        ]);
+      });
     } catch (err) {
       console.log(err);
     }
@@ -63,19 +71,29 @@ const FileUpload = () => {
 
   const handleTypeChange = (e: any) => {
     setType(e.target.value);
-    setImage(undefined);
-    setBase64(undefined);
+    setImage([]);
+    setBase64([]);
   };
 
-  const deleteImage = () => {
-    setBase64(undefined);
-    setImage(undefined);
+  const downloadZip = async () => {
+    const zip = await fetch("http://localhost:5000/convert/zip-download", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ data: image }),
+    });
+    const data = await zip.json();
+    setZipped(data.src);
   };
 
+  const deleteImage = (data: ImageData) => {
+    const filteredBase64 = base64.filter((item) => item.name !== data.name);
+    setBase64(filteredBase64);
+    base64.length == 0 && setImage([]);
+  };
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
-    <div className="flex flex-col items-center justify-center gap-3">
-      <div className="flex flex-row items-center justify-start p-3 gap-2">
+    <div className="flex flex-col items-start justify-center gap-3">
+      <div className="flex flex-row items-center p-3 gap-2 w-full">
         <p>Convert to :</p>
         <select
           name="fileType"
@@ -99,40 +117,67 @@ const FileUpload = () => {
         </p>
       </div>
 
-      <div className="flex flex-row gap-3">
-        {base64 && (
-          <div className="relative flex flex-col items-center justify-center p-2 gap-3">
-            <button
-              className="absolute top-0 right-0 z-50 text-white bg-red-400 w-7 h-7 rounded-full"
-              onClick={deleteImage}
-            >
-              x
-            </button>
-            <Image src={base64.src} alt="" width={100} height={100} />
-            <p>{base64.name}</p>
-          </div>
-        )}
+      <div className="flex flex-row gap-3 flex-wrap">
+        {base64.map((item, key) => {
+          return (
+            <div className="flex flex-col gap-3 justify-between items-center">
+              <div className="relative flex flex-col items-center justify-center p-2 gap-3">
+                <button
+                  className="absolute top-0 right-0 z-50 text-white bg-red-400 w-7 h-7 rounded-full font-semibold"
+                  onClick={() => deleteImage(item)}
+                >
+                  x
+                </button>
+                <Image
+                  src={item.src}
+                  alt={item.name}
+                  width={100}
+                  height={100}
+                />
+                <p className="max-w-[100px] truncate">{item.name}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
 
-      {image ? (
+      {/* {
         <a
           className="p-3 bg-blue-600 rounded-xl text-white font-semibold"
-          href={image}
+          href={image?.src}
           download={base64?.name.split(".")[0]}
         >
           Download
         </a>
-      ) : (
-        <button
-          className={`p-3 ${
-            base64 ? "bg-blue-600" : "bg-blue-200"
-          } rounded-xl text-white font-semibold`}
-          onClick={handleConvert}
-          disabled={!base64}
-        >
-          Convert
-        </button>
-      )}
+      } */}
+
+      <button
+        className={`p-3 ${
+          base64.length ? "bg-blue-600" : "bg-blue-200"
+        } rounded-xl text-white font-semibold`}
+        onClick={() => handleConvert()}
+        disabled={!base64.length}
+      >
+        Convert
+      </button>
+      <button
+        className={`p-3 ${
+          base64.length ? "bg-blue-600" : "bg-blue-200"
+        } rounded-xl text-white font-semibold`}
+        onClick={downloadZip}
+        disabled={!base64.length}
+      >
+        Convert Archive
+      </button>
+      <a
+        className={`p-3 mt-10 ${
+          base64.length ? "bg-blue-600" : "bg-blue-200"
+        } rounded-xl text-white font-semibold`}
+        href={`data:application/zip;base64,${zipped}`}
+        download={"converted.zip"}
+      >
+        Download Archive
+      </a>
     </div>
   );
 };
--- server/routes/convertRouter.js
+++ server/routes/convertRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const sharp = require("sharp");
 const router = express.Router();
 const axios = require("axios");
-
+const JSZip = require("jszip");
 
 router.post("/upload/:type", async (req, res) => {
   try {
@@ -17,7 +17,27 @@ router.post("/upload/:type", async (req, res) => {
     );
     const dataUrl = `data:image/${type};base64,${datastr}`;
     console.log("İmage Çevirildi");
-    return res.json(dataUrl);
+    return res.json({ src: dataUrl });
+  } catch (err) {
+    console.log(err);
+    res.json({ err });
+  }
+});
+
+router.post("/zip-download", async (req, res) => {
+  try {
+    const data = req.body.data;
+    const zip = new JSZip();
+    data.forEach((element) => {
+      console.log(element);
+      zip.file(element.name, element.src.split(",")[1], {
+        base64: true,
+      });
+    });
+    const content = await zip.generateAsync({ type: "base64" });
+    console.log("Sıkıştırıldı");
+
+    return res.json({ src: content });
   } catch (err) {
     console.log(err);
     res.json({ err });
